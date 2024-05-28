ee107a23593551d85866cf36e27a9af9ee7a2add
Ellis Coon <ellis.coon@equusoft.com>
2024-05-28 10:54:23 -0600

    finished db script

--- backend/db.py
+++ backend/db.py
@@ -50,14 +50,22 @@ class TABLES(Enum):
 #
 #
 def table_create_sql(t: TABLES) -> Sql:
-    field_sql = map(field_create_sql, t.value.fields)
-    any_foreign_keys = any(list(map(lambda f: f.isForeignKey, t.value.fields))) 
-    foreign_keys = list(map(lambda f: f.name, filter(lambda f: f.isForeignKey, t.value.fields)))
+    field_sql: map[Sql] = map(field_create_sql, t.value.fields)
+    any_foreign_keys: bool = any(list(map(lambda f: f.isForeignKey, t.value.fields))) 
+    foreign_keys: list[Name] = list(
+        map(lambda f: f.name, 
+            filter(lambda f: f.isForeignKey, t.value.fields)))
+
     return ( 
         f" CREATE TABLE IF NOT EXISTS {t.name} ( "
         f" {", ".join(list(field_sql))} "
-        f" {'FOREIGN KEY (' + ', '.join(foreign_keys) + ')' if any_foreign_keys else ''}"
+        f" {", " + ", ".join(list(map(foreign_key_create_sql, foreign_keys))) if any_foreign_keys else ''}"
         ");")
+
+
+def foreign_key_create_sql(f_name: Name) -> Sql:
+    t_name: Name = f_name.split("_")[0]
+    return f" FOREIGN KEY ({f_name}) REFERENCES {t_name.title()} (id) "
 
 
 def field_create_sql(f: Field) -> Sql:
@@ -67,11 +75,13 @@ def field_create_sql(f: Field) -> Sql:
     return f"{f.name} {f.type} {is_primary_key} {is_nullable} {is_unique} "
 
 
-def main():
-    cur = sqlite3.connect("db.db").cursor()
+#
+#
+# MAIN
+#
+#
+if __name__ == "__main__":
+    cur = sqlite3.connect("database.db").cursor()
     scripts = list(map(table_create_sql, TABLES))
     list(map(cur.executescript, scripts))
-
-if __name__ == "__main__":
-    main()
 
