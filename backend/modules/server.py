c3019d93a53d0ee7faa19d8cd60798adb735f4e0
Ellis Coon <ellis.coon@equusoft.com>
2024-06-18 15:37:24 -0600

    need to run server on home com

--- backend/modules/server.py
+++ backend/modules/server.py
@@ -1,7 +1,8 @@
-from socketserver import TCPServer
 from http.server import BaseHTTPRequestHandler
+from socketserver import TCPServer
 
 from common import IO
+from myhtml import *
 
 #
 #
@@ -21,14 +22,24 @@ type PortNumber = int
 
 class MyHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        if self.path == "/captureImage":
-            print("")
-
-        self.send_response(200)
+        print(f"got a hit at {self.path}")
+        match self.path.split("/"):
+            case ['', '']:
+                self.send_response(200, html((h1("Testing"), h2("testing2"))))
+            case _:
+                self.send_response(400, "where are you trying to go?")
 
 def server_run(n: PortNumber) -> IO:
     """
     Starts up the server.
     """
     TCPServer(("", n), MyHandler).serve_forever()
+
+
+if __name__ == "__main__":
+    server_run(8080)
+
+
+
+
 
