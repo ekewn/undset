227a1ca5a0f99055d7431dde2fc89606e814c470
Ellis Coon <ellis.coon@equusoft.com>
2024-06-18 14:55:12 -0600

    working on html generators

--- backend/modules/html.py
+++ backend/modules/html.py
@@ -1,7 +1,7 @@
-from typing import Iterable, Literal
 from functools import partial
+from typing import Iterable, Literal, Tuple
 
-from common import Fn
+from common import Fn, compose
 
 #
 #
@@ -10,16 +10,21 @@ from common import Fn
 #
 
 
+type Link          = str
 type Html          = str
 type H1            = Html
 type H2            = Html
+type Th            = Html
+type Td            = Html
 type Tr            = Html
-type Tc            = Html
+type Table         = Html
+type A             = Html
 type P             = Html
 type Text          = Html
-type HtmlComponent = H1 | H2 | P | Tr | Tc
+type HtmlComponent = H1 | H2 | P | Tr | Td
 type HtmlTag       = (Literal["h1"] | Literal["h2"] | Literal["p"] |
-                      Literal["tr"] | Literal["tc"])
+                      Literal["tr"] | Literal["td"] | Literal["a"] |
+                      Literal["th"])
 
 
 #
@@ -28,6 +33,9 @@ type HtmlTag       = (Literal["h1"] | Literal["h2"] | Literal["p"] |
 #
 #
 
+def join(i: Iterable[str]) -> str:
+    return "".join(i)
+
 
 def html(hs: Iterable[HtmlComponent]) -> Html:
     return (
@@ -35,7 +43,7 @@ def html(hs: Iterable[HtmlComponent]) -> Html:
         "<!DOCTYPE html>"
         '<html lang="en">'
         "<body>"
-        f"{"".join(hs)}"
+        f"{join(hs)}"
         "</body>"
         "</html>"
     )
@@ -43,8 +51,40 @@ def html(hs: Iterable[HtmlComponent]) -> Html:
 
 def _htmlComponent(t: HtmlTag, s: str | HtmlComponent) -> HtmlComponent:
     return f"<{t}>{s}</{t}>"
-h1:  Fn[str, H1] = partial(_htmlComponent, "h1")
-h2:  Fn[str, H2] = partial(_htmlComponent, "h2")
-tr:  Fn[str, Tr] = partial(_htmlComponent, "tr")
-tc:  Fn[str, H1] = partial(_htmlComponent, "tc")
-p :  Fn[str, H2] = partial(_htmlComponent, "p")
+h1:  Fn[str, H1]   = partial(_htmlComponent, "h1")
+h2:  Fn[str, H2]   = partial(_htmlComponent, "h2")
+p :  Fn[str, P]    = partial(_htmlComponent, "p")
+_th:  Fn[str, Th]  = partial(_htmlComponent, "th")
+_td:  Fn[str, Td]  = partial(_htmlComponent, "td")
+__tr:  Fn[str, Tr] = partial(_htmlComponent, "tr")
+
+_tr = lambda x: compose(partial(map,x), join, __tr)
+
+
+def th(headers: Iterable[str]) -> Tr:
+    return _tr(_th)(headers)
+
+
+def td(rows: Iterable[str]) -> Tr:
+    return _tr(_td)(rows)
+
+
+def a(content: str, link: Link) -> A:
+    return f"<a href={link}>{content}</a>"
+
+
+def table(headers: Iterable[Td], rows: Iterable[Tuple]) -> Table:
+    return f"<table>{th(headers)}{join(map(td,rows))}</table>"
+
+
+#
+#
+# TEST
+#
+#
+if __name__ == "__main__":
+    print_html = compose(html, print)
+    print_html(table(["h1", "h2", "h3"]
+                      , [("td11", "td12", "td13"),
+                         ("td21", "td22", "td23")]))
+
