14b95dd58f4002cbf1fcccd3dd4eea39a72c24bd
Ellis Coon <ellis.coon@gmail.com>
2024-06-23 16:17:42 -0600

    needs some testies

--- backend/modules/common.py
+++ backend/modules/common.py
@@ -1,6 +1,6 @@
 from collections import deque
 from functools import partial, reduce
-from itertools import accumulate, count, islice, repeat
+from itertools import accumulate, count, filterfalse, islice, repeat, tee
 from operator import *
 from typing import Any, Callable, Dict, Iterable, Iterator, List, Optional, Tuple
 
@@ -34,6 +34,7 @@ def map_[a, b](fn: Callable[[a], b]) -> Callable[[Iterable[a]], Iterable[b]]:
     Curried map.
     """
     return partial(map, fn)
+# NOTE: Needs test
 
 
 def filter_[a](p: Predicate[a]) -> Callable[[Iterable[a]], Iterable[a]]:
@@ -41,7 +42,7 @@ def filter_[a](p: Predicate[a]) -> Callable[[Iterable[a]], Iterable[a]]:
     Curried filter.
     """
     return partial(filter, p)
-
+# NOTE: Needs test
 
 # Composers
 
@@ -60,6 +61,7 @@ def pipe(val, *funcs: Callable):
     Applies the functions to the value from left to right.
     """
     return compose(*funcs)(val)
+# NOTE: Needs test
 
 
 # Composition Helpers
@@ -71,18 +73,19 @@ def id[a](x: a) -> a:
     return x
 
 
-def tap[a](fn: Callable, x: a) -> a:
-    """
-    Calls a function and then returns the argument.
-    """
-    return compose(fn, id)(x)
-
-
 def always[a](x: a) -> FnN[a]:
     """
     Returns a function that always returns the arg.
     """
     return partial(id, x)
+# NOTE: Needs test
+
+
+def tap[a](fn: Callable, x: a) -> a:
+    """
+    Calls a function and then returns the argument.
+    """
+    return compose(fn, id)(x)
 
 
 # Logical
@@ -92,6 +95,7 @@ def T(*args) -> bool:
     Always returns true.
     """
     return True
+# NOTE: Needs test
 
 
 def F(*args) -> bool:
@@ -99,6 +103,7 @@ def F(*args) -> bool:
     Always returns False.
     """
     return False
+# NOTE: Needs test
 
 
 def both[a](p1: Predicate[a], p2: Predicate[a]) -> Predicate[a]:
@@ -107,6 +112,7 @@ def both[a](p1: Predicate[a], p2: Predicate[a]) -> Predicate[a]:
     """
     def _(x, y, arg) -> bool: return and_(x(arg), y(arg))
     return partial(_, p1, p2)
+# NOTE: Needs test
 
 
 def either[a](p1: Predicate[a], p2: Predicate[a]) -> Predicate[a]:
@@ -115,6 +121,7 @@ def either[a](p1: Predicate[a], p2: Predicate[a]) -> Predicate[a]:
     """
     def _(x, y, arg) -> bool: return or_(x(arg), y(arg))
     return partial(_, p1, p2)
+# NOTE: Needs test
 
 
 # Branches
@@ -133,6 +140,7 @@ def unless[a, b](p: Predicate[a], fn: FnU[a, b]) -> FnU[a, a | b]:
     """
     def _(p, f, v): return f(v) if not_(p(v)) else v
     return partial(_, p, fn)
+# NOTE: Needs test
 
 
 def when[a, b](p: Predicate[a], fn: FnU[a, b]) -> FnU[a, a | b]:
@@ -141,15 +149,20 @@ def when[a, b](p: Predicate[a], fn: FnU[a, b]) -> FnU[a, a | b]:
     """
     def _(p, f, v): return f(v) if p(v) else v
     return partial(_, p, fn)
+# NOTE: Needs test
 
 
 type IfThens[a, b] = Tuple[Predicate[a], FnU[a, b]]
 def cond[a, b](if_thens: List[IfThens[a, b]]) -> FnU[a, Optional[b]]:
+    """
+    Returns a unary function that applies the first function whose predicate is satisfied.
+    """
     def _(its: List[IfThens[a, b]], arg: a):
         for it in its:
             if it[0](arg):
                 return it[1](arg)
     return partial(_, if_thens)
+# NOTE: Needs test
 
 
 def const[a](x: a) -> Callable[[Any], a]:
@@ -158,6 +171,7 @@ def const[a](x: a) -> Callable[[Any], a]:
     """
     def _(val, ignore): return val      # "Ignore is not accessed"... that's the point
     return partial(_, x)
+# NOTE: Needs test
 
 
 def default_to[a](default: a, val: a) -> a:
@@ -165,14 +179,16 @@ def default_to[a](default: a, val: a) -> a:
     Returns default value if val is None.
     """
     return default if val is None else val
+# NOTE: Needs test
 
 
-def with_default[a, b](default: b, fn: FnU[a, Optional[b]]) -> FnU[a, b]:
+def default_with[a, b](default: b, fn: FnU[a, Optional[b]]) -> FnU[a, b]:
     """
     Returns a function that will return the default value if the result is null.
     """
     def _(d, f, v): return d if f(v) is None else f(v)
     return partial(_, default, fn)
+# NOTE: Needs test
 
 
 # Container-related
@@ -191,6 +207,7 @@ def empty[a: (List, Dict, int, str)](x: a) -> a:
         return 0 #type:ignore
     else:
         return "" #type:ignore
+# NOTE: Needs test
 
 
 def is_empty[a: (List, Dict, int, str)](x: a) -> bool:
@@ -198,6 +215,7 @@ def is_empty[a: (List, Dict, int, str)](x: a) -> bool:
     Checks if value is the identity value of the monoid.
     """
     return any([x == [], x == {}, x == "", x == 0])
+# NOTE: Needs test
 
 
 def is_nil(x: Any) -> bool:
@@ -205,6 +223,7 @@ def is_nil(x: Any) -> bool:
     Checks if value is None.
     """
     return is_(x, None)
+# NOTE: Needs test
 
 
 # Iterator Specifics
@@ -216,18 +235,36 @@ def consume(i: Iterator) -> None:
     deque(i, maxlen=0)
 
 
-def take[a](n: int, i: Iterator[a]) -> List[a]:
+def take[a](n: int, i: Iterator[a]) -> Iterator[a]:
     """
-    Takes the first n from an iterator.
+    Returns an iterator of the first n items from the supplied iterator.
     """
-    return list(islice(i, n))
+    return islice(i, n)
+# NOTE: Needs test
+
+
+def head[a](i: Iterator[a]) -> a:
+    """
+    Gets first item from an iterator.
+    """
+    return next(i)
+# NOTE: Needs test
 
 
 def drop[a](n: int, i: Iterator[a]) -> Iterator[a]: 
     """
-    Drops the first n from an iterator.
+    Drops the first n items from an iterator.
     """
     return islice(i, n, None)
+# NOTE: Needs test
+
+
+def tail[a](i: Iterator[a]) -> Iterator[a]:
+    """
+    Returns an iterator without the first element of the given iterator.
+    """
+    return drop(1, i)
+# NOTE: Needs test
 
 
 def iterate[a](fn: Callable[[a], a], x: a) -> Iterator[a]:
@@ -235,6 +272,15 @@ def iterate[a](fn: Callable[[a], a], x: a) -> Iterator[a]:
     Creates an iterator by applying the same function to the result of f(x).
     """
     return accumulate(repeat(x), lambda fx, _ : fn(fx))
+
+
+def partition[a](p: Predicate[a], i: Iterable[a]) -> Tuple[Iterator[a], Iterator[a]]:
+    """
+    Returns the iterable separated into those that satisfy and don't satisfy the predicate.
+    """
+    t1, t2 = tee(i)
+    return filter(p, t1), filterfalse(p, t2)
+# NOTE: Needs test
 
 
 # List Functions
@@ -246,18 +292,15 @@ def adjust[a](idx: int, fn:FnU[a, a], l: List[a]) -> List[a]:
     l2 = l.copy()
     l2[idx] = fn(l2[idx])
     return l2
-
-
-def head[a](i: List[a]) -> a:
-    """
-    Gets first item from a list without mutating.
-    """
-    return itemgetter(0)(i)
+# NOTE: Needs test
 
 
 # Dictionary Functions
 
 def get[a, b](d: Dict[a, b], default: b, key: a) -> b:
+    """
+    Dict.get alias.
+    """
     return methodcaller("get", key, default)(d)
 
 
@@ -270,8 +313,8 @@ def get[a, b](d: Dict[a, b], default: b, key: a) -> b:
 
 if __name__ == "__main__":
     assert compose(len, lambda x: x + 10, lambda y: y - 1)("number should be 28") == 28
-    assert take(4, iterate(partial(add, 3),2)) == [2, 5, 8, 11]
-    assert take(3, drop(2, count())) == [2, 3, 4]
+    assert list(take(4, iterate(partial(add, 3),2))) == [2, 5, 8, 11]
+    assert list(take(3, drop(2, count()))) == [2, 3, 4]
     assert if_else(lambda _: True, lambda _: "a", lambda _: "b")("") == "a"
     assert if_else(lambda _: False, lambda _: "a", lambda _: "b")("") == "b"
     assert id("1") == "1"
