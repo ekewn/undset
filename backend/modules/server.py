c7c468d18d477ba6d81f4b4e967dad46753d1bf6
Ellis Coon <ellis.goon@gmail.com>
2024-07-06 16:26:53 -0600

    simplified server

--- backend/modules/server.py
+++ backend/modules/server.py
@@ -1,9 +1,8 @@
 from http.server import BaseHTTPRequestHandler
-from operator import attrgetter
 from socketserver import TCPServer
-from typing import BinaryIO, Callable, Dict
+from typing import Dict
 
-from pyamda import IO, get, tap
+from pyamda import IO
 from htmlgen import *
 
 #
@@ -13,11 +12,9 @@ from htmlgen import *
 #
 
 
-type Path = str
-type ResponseCode = int
-type PortNumber = int
 type Route = str
 type RouteFunctions = Dict[Route, Html]
+
 
 ROUTEFUNCTIONS: RouteFunctions = {"/": html((h1("Testing"), h2("testing2"))),
                                   "404": html(h1("Where were you trying to go?"))}
@@ -28,36 +25,13 @@ ROUTEFUNCTIONS: RouteFunctions = {"/": html((h1("Testing"), h2("testing2"))),
 #
 #
 
-
-# Header-Related
-def _send_response(rc: ResponseCode, self: BaseHTTPRequestHandler) -> IO: return methodcaller("send_response", rc)(self)
-_send_200    : FnU[BaseHTTPRequestHandler, BaseHTTPRequestHandler] = partial(tap, partial(_send_response, 200))
-_send_400    : FnU[BaseHTTPRequestHandler, BaseHTTPRequestHandler] = partial(tap, partial(_send_response, 400))
-
-_send_headers: FnU[BaseHTTPRequestHandler, BaseHTTPRequestHandler] = partial(tap, methodcaller("send_header", "Content-Type", "text/html; charset = utf-8"))
-_end_headers : FnU[BaseHTTPRequestHandler, BaseHTTPRequestHandler] = partial(tap, methodcaller("end_headers"))
-
-# Write-Related
-_wfile       : FnU[BaseHTTPRequestHandler, BinaryIO] = attrgetter("wfile")
-_write       : FnU[BinaryIO, Callable[[bytes], IO]]  = partial(methodcaller("write"), _wfile)
-_path        : FnU[BaseHTTPRequestHandler, Path]     = attrgetter("path")
-_bytes_utf8  : FnU[str, bytes]                       = partial(bytes, encoding = "utf-8")
-_call_rf     : FnU[Route, Html]                      = partial(get, ROUTEFUNCTIONS, "404")
-
-# Pipelines
-init_response = compose(_send_200, _send_headers, _end_headers)
-write_html    = compose(_path, _call_rf, _bytes_utf8, _write)
-
-
 # Handler
 class MyHandler(BaseHTTPRequestHandler):
     def do_GET(self) -> IO:
-        init_response(self)
-        write_html(self)
-
-
-def server_run(n: PortNumber) -> IO:
-    TCPServer(("", n), MyHandler).serve_forever()
+        self.send_response(200)
+        self.send_header("Content-Type", "text/html; charset = utf-8")
+        self.end_headers()
+        self.wfile.write(bytes(ROUTEFUNCTIONS.get(self.path, "404"), "utf-8"))
 
 
 #
@@ -68,7 +42,4 @@ def server_run(n: PortNumber) -> IO:
 
 
 if __name__ == "__main__":
-    server_run(8080)
-
-
-
+    TCPServer(("", 8080), MyHandler).serve_forever()
